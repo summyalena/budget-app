923d0de27ae1bea4c2006b08c615e7403f166ad7
Lena <summyalena@gmail.com>
2023-01-06 15:00:27 +0100

    linter error

--- db/migrate/20221218732033_add_operation_ref_to_money_groups.rb
+++ db/migrate/20221218732033_add_operation_ref_to_money_groups.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class AddOperationRefToMoneyGroups < ActiveRecord::Migration[7.0]
   def change
--- db/migrate/20221219140332_add_devise_to_users.rb
+++ db/migrate/20221219140332_add_devise_to_users.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
